5cd5c02d7311b718a830aa9615ad52f6ce674005
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-08-19 21:24:19 -0300

    feat: muda o globalStyles fora do myapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { globalStyles } from "@/styles/global"
 import { AppProps } from "next/app"
 
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles()
-  
   return <Component {...pageProps} />
 }
