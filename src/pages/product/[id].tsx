3925651b2b649ab3aebd1adb99cf478cbc3549fa
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-08-26 15:31:29 -0300

    feat: cria pagina de detalhes do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetail } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
   return (
-    <h1>Products: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetail>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quam deserunt sed maiores odio laboriosam ipsam? Consectetur earum deserunt dolores, corporis similique cumque voluptates quae deleniti rerum aut perspiciatis unde?</p>
+
+        <button>Comprar agora</button>
+      </ProductDetail>
+    </ProductContainer>
   )
 }
